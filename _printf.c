0404b0f4fb901ce8486942b191e2a8167037bc62
Youssef-Mafa <125815717+Youssef-Mafa@users.noreply.github.com>
2023-04-25 17:58:19 +0000

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,35 +1,31 @@
 #include "main.h"
-
 /**
- * _printf - prints formatted output to stdout
- * @format: format string
- * 
- * Return: number of characters printed, or -1 if an error occurred
+ * _printf - prints according to a format
+ * @format: pointer to str contain zero
+ * Return: number of character printed
  */
 int _printf(const char *format, ...)
 {
-    if (format == NULL) return -1;
-    
-    va_list args;
-    va_start(args, format);
+	int i = 0, c = 0;
+	va_list arg;
 
-    int count = 0;
-    for (const char *p = format; *p != '\0'; p++)
-    {
-        if (*p != '%')
-        {
-            putchar(*p);
-            count++;
-        }
-        else if (*(p + 1) != '\0')
-        {
-            int res = handle_format_specifier(&p, args);
-            if (res < 0) return -1;
-            count += res;
-        }
-        else return -1;
-    }
-
-    va_end(args);
-    return count;
+	va_start(arg, format);
+	if (format == NULL)
+		return (-1);
+	for (i = 0; *(format + i) != '\0'; i++)
+	{
+		if (*(format + i) == '%')
+		{
+			if (format[i + 1] == '\0')
+				return (-1);
+			c += _printf_assist(format, arg, &i);
+		}
+		else
+		{
+			putchar(*(format + i));
+			c++;
+		}
+	}
+	va_end(arg);
+	return (c);
 }
